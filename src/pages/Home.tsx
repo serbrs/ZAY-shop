ca504dd23d93790a85e389f9f5ccc5d3631edc2d
thecodefatheer <alicenab13@gmail.com>
2022-02-20 17:28:02 +0400

    created home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Layout from "../components/Layout";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <Layout />
+      <Footer />
+    </div>
+  );
 };
-
 export default Home;
